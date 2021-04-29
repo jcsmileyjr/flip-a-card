d36ae2020a8bdeb0866a52b9f14aff07c3751894
Smiley <jcsmileyjr@gmail.com>
2021-04-29 06:37:31 -0500

    Setup Grid Styling and initial responsive design

--- src/App.js
+++ src/App.js
@@ -3,9 +3,14 @@ import './App.css';
 
 function App() {
   return (
-    <div className="">
-      Hello world
-    </div>
+    <main className="App">
+      <section className="title">Title</section>
+      <section className="info">Info</section>
+      <section className="art1">Art1</section>
+      <section className="game">Game</section>
+      <section className="art2">Art2</section>
+      <section className="restart">Restart</section>
+    </main>
   );
 }
 
