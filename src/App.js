ebecca37923d8dfdfba598147a2b9cc1d977f6bd
Smiley <jcsmileyjr@gmail.com>
2021-04-29 20:55:38 -0500

    Updated card styles and etc.

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import playingCards from './data/cards';
 import Card from './components/Card/Card';
 import Score from './components/Score/Score';
 import Timer from './components/Timer/Timer';
+import cartoonMan from './assets/clumsy.png';
+import cartoonLady from './assets/laying.png';
+import Restart from './components/Restart/restart';
 
 function App() {
 
@@ -22,12 +25,18 @@ function App() {
         <Score score="100" />
         <Timer time="60" />
       </section>
-      <section className="art1">Art1</section>
+      <section className="art1">
+      <img src={cartoonLady} className="art__style floatRight" alt="cartoon lady laying down" />
+      </section>
       <section className="game">
         {gameArea}
       </section>
-      <section className="art2">Art2</section>
-      <section className="restart">Restart</section>
+      <section className="art2">
+        <img src={cartoonMan} className="art__style" alt="cartoon man dropping cards" />
+      </section>
+      <section className="restart">
+        <Restart />
+      </section>
     </main>
   );
 }
---
+++ src/components/Restart/restart.js
@@ -0,0 +1,11 @@
+import './restart.css';
+
+const Restart = () => {
+    return(
+        <main>
+            <button type="button" className="button__style">Restart</button>
+        </main>
+    )
+};
+
+export default Restart;
