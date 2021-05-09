ff26be593771985bec7c0a7ac3d4976ee978b447
Smiley <jcsmileyjr@gmail.com>
2021-05-09 10:22:19 -0500

    Accessibility enhancements
    1. Add tabbing
    2. Flip card via the keyboard "enter" key"
    3. Add color outline to show focus
    4. Add color outline to show

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -3,9 +3,12 @@ import './card.css';
 import React from 'react';
 
 const Card = props => {
+    const flipCardViaEnter = () => {
+        props.flipCard();
+    }
 
     return(
-        <article className="card__style front" onClick={()=> {props.flipCard()}}>
+        <article tabIndex="0" className="card__style front" onKeyPress={() => {flipCardViaEnter()}} onClick={()=> {props.flipCard()}}>
             <img aria-label="Clickable card that shows an image when clicked" src={props.image} className={`image ${props.correct ? "showImage" : "hideImage"}`} alt="" />
         </article>
     );
